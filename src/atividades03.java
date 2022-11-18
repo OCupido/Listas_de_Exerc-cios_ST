e64c2b6e9a6baac3a7c22bf3157a03610e19a903
José <ricardomafrajose@gmail.com>
2022-11-18 16:31:57 -0300

    puchando exercicios 1,2 e 3

---
+++ src/atividades03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class atividades03 {
+    public static void main (String[] args) {
+        int contatdor = 0;
+        while (contatdor<=9){
+            System.out.println(contatdor);
+            contatdor++;
+        }
+        System.out.println(contatdor + "\n");
+
+        //1- Números ímpares
+
+        System.out.println("Digite um Numero: ");
+        Scanner num = new Scanner(System.in);
+        int numero = num.nextInt();
+
+    }
+}
